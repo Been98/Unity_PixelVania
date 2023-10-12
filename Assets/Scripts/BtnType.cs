8aa745f45ac009b47667cf624e2926c2b36c9d36
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2023-10-12 18:12:40 +0900

    Mainmenu Scripts
    Mainmenu Scripts

---
+++ Assets/Scripts/BtnType.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnType : MonoBehaviour
+{
+    public BTNType currentType;
+
+
+    void Start()
+    {
+
+    }
+
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNType.Start:
+                SceneManager.LoadScene("Level 1");
+                break;
+            case BTNType.Quit:
+                ClickQuit();
+                break;
+
+        }
+    }
+
+    void ClickQuit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum BTNType
+{
+    Start,
+    Option,
+    Sound,
+    Back,
+    Quit,
+}
+
+public class MainMenu : MonoBehaviour
+{
+    
+}
