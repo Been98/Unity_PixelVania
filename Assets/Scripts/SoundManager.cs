050bd09a751dc62d4946a26f8e682371b2a9887b
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2023-10-12 19:42:03 +0900

    Sound
    Sound 추가

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource musicsource;
+
+    public void SetMusicVolume(float volume)
+    {
+        musicsource.volume = volume;
+
+    }
+
+
+
+
+}
